3e3ff966b8e2d80d38ab073b98642d2fd7c0ef73
Michael Davis <mdavis3@godaddy.com>
2021-11-19 07:50:38 -0700

    Start implementation of better generation algorithm

--- smake.py
+++ smake.py
@@ -38,5 +38,65 @@ def gen() :
                 stack.append(puzzle)
                 break
 
+def genfrom(puzzle) :
+    # end recursion, check if we found a valid solution
+    #
+    if puzzle.count('.') == 81-17 :
+        solns = [x for _,x in zip(range(2), ssolve.solutions(puzzle))]
+        if len(solns) == 1 :
+            return puzzle
+        else :
+            print('bottomed out unsuccessfully with {}'.format(puzzle))
+            return None
+
+    # we can remove at least one item, so recurse over each possibility
+    # until one is found
+    indexes = list(range(81))
+    random.shuffle(indexes)
+    for x in indexes:
+        if puzzle[x] != '.' :
+            next = ssolve.substitute('.', x, puzzle)
+
+            solns = [x for _,x in zip(range(2), ssolve.solutions(next))]
+            if len(solns) == 1 :
+                # exactly one solution, this one maybe could get shorter
+                test = genfrom(next)
+                if test :
+                    return test
+            else :
+                # this one is ambiguous or inconsistent, skip it
+                print('ambiguity with {}'.format(next))
+                pass
+
+    return None
+
+def dotify(puzzle, indexes) :
+    next = ['.'] * 81
+
+    for x in range(81) :
+        if x not in indexes :
+            next[x] = puzzle[x]
+
+    return ''.join(next)
+
+def genfrom2(puzzle) :
+    indexes = list(range(81))
+    while True :
+        random.shuffle(indexes)
+        next = dotify(puzzle, indexes[:64])
+        solns = [x for _,x in zip(range(2), ssolve.solutions(next))]
+        if len(solns) == 1 :
+            # exactly one solution, winner!
+            return next
+
+#
+# supposedly better algorithm.  start with a completed puzzle
+# and remove until you can't remove any more.  min length is 17.
+#
+def gen2() :
+    puzzle = [x for _,x in zip(range(1), ssolve.solutions('.' * 81))][0] ## start with any puzzle
+    return genfrom2(puzzle)
+
 if __name__ == '__main__' :
-    ssolve.prettyprint(gen())
+    #ssolve.prettyprint(gen())
+    ssolve.prettyprint(gen2())
