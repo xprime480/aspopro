7b931af89deae8f2084c56417358250772b7e699
Michael Davis <mdavis3@godaddy.com>
2022-01-20 09:45:18 -0700

    refactor into a function and driver

--- qik.py
+++ qik.py
@@ -1,14 +1,20 @@
 import sys
 import ong
 
-stop = 170
-start = 1
-if len(sys.argv) > 1:
-    stop = int(sys.argv[1])
-if len(sys.argv) > 2:
-    start = int(sys.argv[2])
+def qik(start, stop) :
+    v0 = [x for x in range(start,stop+1)]
+    return ong.ong(v0)
 
-v0 = [x for x in range(start,stop+1)]
-v1 = ong.ong(v0)
-for v in v1:
-    print(v[0], v[1])
+if __name__ == '__main__' :
+    start = 1
+    stop = 170
+
+    if len(sys.argv) > 1:
+        stop = int(sys.argv[1])
+    if len(sys.argv) > 2:
+        start = int(sys.argv[2])
+
+    v1 = qik(start, stop)
+    for v in v1:
+        ##print(v[0], v[1])
+        print("({}, {})".format(*v))
